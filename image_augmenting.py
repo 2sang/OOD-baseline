23a4137f39f1c7fd3c9f76b3f02db9d5a96b209d
2sang <sangsulee92@gmail.com>
2018-07-17 17:39:28 +0900

    Almost done

--- image_augmenting.py
+++ image_augmenting.py
@@ -1,9 +1,24 @@
 import numpy as np
 from skimage.filters import gaussian
 
+distortion = np.random.uniform(low=0.9, high=1.2)
+
 def add_noise(batch, complexity=0.5):
     return batch + np.random.normal(size=batch.shape, scale=1e-9 + complexity)
 
-def blur(img, complexity=0.5):
+def add_distortion_noise(batch):
+    return batch + np.random.normal(size=batch.shape, scale=1e-9 + distortion)
+
+def add_distortion_blur(img):
     image = img.reshape((-1, 28, 28))
-    return gaussian(image, sigma=5*complexity).reshape((-1, 28*28))
+    return gaussian(image, sigma=5*distortion).reshape((-1, 28*28))
+
+def rotate90_if_not_zero(batch, batch_label):
+    mask = batch_label != 0
+    notzeros = batch[mask]
+    notzeros = np.rot90(notzeros.reshape((-1, 28, 28))).reshape((-1, 28*28))
+    zeros = batch[np.invert(mask)]
+    zeros = add_noise(zeros, complexity=1)
+    batch = np.vstack((notzeros, zeros))
+    return batch
+
